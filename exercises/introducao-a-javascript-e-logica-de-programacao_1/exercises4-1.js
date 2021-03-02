d846789da7a632bf0a6ea1475cdda9be4aabc810
Anibal <anibalneto29@gmail.com>
2021-03-01 23:42:30 -0300

    2 e 3 concluidos (real oficial)

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises4-1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises4-1.js
@@ -11,8 +11,27 @@ console.log('Módulo: ' + (a % b));
 
 // Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados. //
 
+let a = 5;
+let b = 9;
+
 if (a > b) {
     console.log('A é maior que B')
 } else {
     console.log('B é maior que A')
+}
+
+// Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados. //
+
+let numbers = {
+    a: 3,
+    b: 7,
+    c: 5
+}
+
+if (numbers['a'] > numbers['b'] && numbers['a'] > numbers['c']) {
+    console.log('O maior número é: ' + numbers['a'] + ' (a)')
+} else if (numbers['b'] > numbers['a'] && numbers['b'] > numbers['c']) {
+    console.log('O maior número é: ' + numbers['b'] + ' (b)')
+} else {
+    console.log('O maior número é: ' + numbers['c'] + ' (c)')
 }
