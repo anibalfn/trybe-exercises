e09b8654e0ac9a52dadfca0d290fb4c3359b8af6
Anibal <anibalneto29@gmail.com>
2021-03-01 23:41:37 -0300

    2 e 3 concluidos

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises4-1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises4-1.js
@@ -1,8 +1,18 @@
+// Operações Aritméticas //
+
 let a = 7;
 let b = 5;
 
 console.log('Soma: ' + (a + b));
 console.log('Subtração: ' + (a - b));
 console.log('Multiplicação: ' + (a* b));
-console.log('Divisão: ' + (a / b));
+console.log('Divisão: ' + (a / b)); 
 console.log('Módulo: ' + (a % b));
+
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados. //
+
+if (a > b) {
+    console.log('A é maior que B')
+} else {
+    console.log('B é maior que A')
+}
