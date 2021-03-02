a8ec0a2a5323767579e96db563be90d1e2921c00
Anibal <anibalneto29@gmail.com>
2021-03-02 00:07:15 -0300

    5 concluido

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises4-1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises4-1.js
@@ -48,3 +48,28 @@ if (a > 0) {
     console.log('zero')
 }
 
+// Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.//
+
+//Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.//
+
+//Um ângulo será considerado inválido se não tiver um valor positivo.//
+
+let degreeAngle = {
+    a: 90,
+    b: 60,
+    c: 30
+}
+
+let angleTotal = degreeAngle.a + degreeAngle.b + degreeAngle.c;
+
+let areAllAnglesPositive = degreeAngle.a + degreeAngle.b + degreeAngle.c > 0;
+
+if (areAllAnglesPositive) {
+    if (angleTotal === 180) {
+        console.log('true')
+    } else {
+        console.log('false')
+    };
+} else {
+    console.log('erro: ângulo inválido')
+}
