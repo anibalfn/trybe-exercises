3505e15ac6f6e5d6db9173d3e6f12d7d6bd3514f
Anibal <anibalneto29@gmail.com>
2021-03-05 22:25:22 -0300

    quarto exercício concluído

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises4-2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises4-2.js
@@ -23,13 +23,19 @@ let average = sum / numbers.length;
 
 console.log(average);
 
-
-
-// A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
-
-
-
 // Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let value = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    value = numbers[index];
+}
+let m = value / numbers.length;
+    if (m > 20) {
+        console.log('Valor maior que 20')
+} else {
+    console.log('Valor menor ou igual a 20')
+}
+
 
 
 
