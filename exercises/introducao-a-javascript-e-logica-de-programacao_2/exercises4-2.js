03a9fd2537c7e9248310738010fc0e6476aa5b51
Anibal <anibalneto29@gmail.com>
2021-03-05 22:56:22 -0300

    nono exercício concluído

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises4-2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises4-2.js
@@ -85,6 +85,9 @@ for (let index = 1; index <= 25; index += 1) {
 }
 console.log(numbersResult)
 
-
-
 // Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+divisionResult = 0;
+for (let index = 0; index < numbersResult.length; index += 1) {
+    console.log(numbersResult[index] / 2);
+}
