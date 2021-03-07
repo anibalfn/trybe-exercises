23a6b0d4a118fd2e1c3707f24433180368efb900
Anibal <anibalneto29@gmail.com>
2021-03-06 23:41:33 -0300

    exercício 1 concluído

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise_1.js
@@ -0,0 +1,11 @@
+let n = 5;
+let symbol = '*';
+let inputSpace = '';
+
+for (let index = 0; index < n; index += 1) {
+    inputSpace = inputSpace + symbol;
+}
+
+for (let index = 0; index < n; index += 1) {
+    console.log(inputSpace);
+};
